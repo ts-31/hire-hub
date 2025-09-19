e35501db1d40b05c0919cc7cde93fa1346c3b53e
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-09-19 12:57:57 +0530

    chore(workspace/backend): remove old protected route used for testing

--- server/app/routers/protected.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from app.auth.dependencies import verify_token
-
-router = APIRouter()
-
-
-@router.get("/protected")
-def protected_route(user_data: dict = Depends(verify_token)):
-    return {"message": "Token is valid!", "user_id": user_data["uid"]}
